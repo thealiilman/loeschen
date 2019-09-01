cf8a6126357172a75c7e5af2a564de2d7679abd4
thealiilman <ali@ali-ilman.com>
2019-09-01 10:43:53 +0800

    Created Books table.

---
+++ db/migrate/20190901024257_create_books.rb
@@ -0,0 +1,8 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.references :author, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_024201) do
+ActiveRecord::Schema.define(version: 2019_09_01_024257) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
 end
